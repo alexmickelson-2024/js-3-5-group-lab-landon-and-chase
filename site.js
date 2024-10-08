ac71db44c659718bc49a13feed314d3fe9aefa58
Chase Harmer <chase.harmer@students.snow.edu>
2024-10-08 12:58:45 -0600

    almost complete code

--- site.js
+++ site.js
@@ -1,22 +1,58 @@
 import { bookList } from "./books.js";
 
+const fliterInputElement = document.getElementById("filterInput")
+var filteredBookList;
+
+const setFilter = () => {
+  console.log(fliterInputElement.value)
+  filteredBookList = 
+}
+
+fliterInputElement.addEventListener("input", setFilter);
 
 const drawBooks = (books) => {
-  const bookHTML = (book) => `
-    <tr>
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.summary}</td>
-    </tr>
-  `
+  const bookNode = (book) => {
+    const tableRowElement = document.createElement("tr")
+    tableRowElement.id = book.title
+
+    const tableDataTitle = document.createElement("td")
+    const tableDataAuthor = document.createElement("td")
+    const tableDataSummary = document.createElement("td")
+
+
+    tableDataTitle.textContent = book.title
+    tableDataAuthor.textContent = book.author
+    tableDataSummary.textContent = book.summary
+
+    tableRowElement.append(tableDataTitle, tableDataAuthor, tableDataSummary)
+    
+    const id = book.title;
+
+    tableRowElement.addEventListener("click", () => {
+      const selectedBookNode = document.getElementById("selectedBookTitle");
+      selectedBookNode.textContent = id
+    })
+
+    return tableRowElement
+  }
+  //   <tr>
+  //     <td>${book.title}</td>
+  //     <td>${book.author}</td>
+  //     <td>${book.summary}</td>
+  //   </tr>
+
 
   const tableBodyElement = document.getElementById("myTableBody")
-  tableBodyElement.innerHTML = "";
 
   books.forEach(book => {
-    const firstBookHtml = bookHTML(book)
-    tableBodyElement.innerHTML += firstBookHtml;
+    tableBodyElement.append(bookNode(book))
   })
 }
 
-drawBooks(bookList);
+console.log(filteredBookList);
+
+if (filteredBookList)
+  drawBooks(filteredBookList)
+
+else
+  drawBooks(bookList)
